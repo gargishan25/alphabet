0233f98d20ca80fbf7eb6599caf5b742cd571566
fullycharged25 <gargishan2010@gmail.com>
2025-11-04 18:29:21 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-x = input("Enter a string (could be anything that is one length long, for example even exclamation point). ")
+x = input("Enter a string (could be anything that is one length long and not a number, for example even exclamation point). ")
 if x.lower() =="a"or x.lower() =="b"or x.lower() =="c"or x.lower() =="d"or x.lower() =="e"or x.lower() =="f"or x.lower() =="g"or x.lower() =="h"or x.lower() =="i"or x.lower() =="j"or x.lower() =="k"or x.lower() =="l"or x.lower() =="m"or x.lower() =="n"or x.lower() =="o"or x.lower() =="p"or x.lower() =="q"or x.lower() =="r"or x.lower() =="s"or x.lower() =="t"or x.lower() =="u"or x.lower() =="v"or x.lower() =="w"or x.lower() =="x"or x.lower() =="y"or x.lower() =="z":
     print("The string is an alphabet.")
 else:
